7233d20926437746661af4ea84c072eb1f1cb694
Miriam McNamara <miriammcnamara@Miriams-Prime-Academy-Computer.local>
2021-10-26 16:16:46 -0500

    images added, data.js updated

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/iclimbtrees.jpg', description: 'I love to climb trees! This taken at Lake Harriet Rose Garden.', likes: 0 }
+    id: 2, path: 'images/iwritebooks.JPG', description: 'I love to write! This is me with my second book, taken at a local bookstore.', likes: 0 }
+    id: 3, path: 'images/polemedalist.JPG', description: 'I love to pole dance! This is me at a pole competition in Chicago, where I took first place!', likes: 0 }
+    id: 4, path: 'images/meandmom.JPG', description: 'Me and my mom look nothing alike, do we?', likes: 0 }
+    id: 5, path: 'images/mypups.jpg', description: 'I have the cutest fur babies in the whole wide world.', likes: 0 }
+    id: 6, path: 'images/mysweetie.jpg', description: 'My sweetie is an art therapist. We made this sign to hang in our window during the pandemic.', likes: 0 }
 ];
 
 module.exports = galleryItems;
